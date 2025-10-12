e51f765f643f9236bfc325cd32eb4168b33d9304
samuelehalaiye-tech <samuelehalaiye@gmail.com>
2025-10-13 00:24:34 +0100

    Added Navbar componenet and added screens to the driver profile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,29 @@
 import React from 'react'
 import { useRouter } from 'expo-router'
 import { View,Pressable,Text,StyleSheet } from 'react-native'
-function home() {
+import { isProtectedReactElement } from 'expo-router/build/views/Protected'
+import { Ionicons } from "@expo/vector-icons";
+
+
+
+function Navbar() {
     const router = useRouter()
   return (
     <View>
         <Pressable onPress={()=> router.push("/driver/Home")}>
-
+            <Ionicons name='home' size={26} color='orange'/>
         </Pressable>
         <Pressable onPress={()=> router.push("/driver/Offers")}>
-
+            <Ionicons name='bookmarks' size={26} color='orange'/>
         </Pressable>
         <Pressable onPress={()=> router.push("/driver/Profile")}>
-            
+            <Ionicons name='person' size={26} color='orange'/>
         </Pressable>
         <Pressable onPress={()=> router.push("/driver/AboutUs")}>
-            
+            <Ionicons name='add-circle'size={26} color='orange'/>
         </Pressable>
     </View>
   )
 }
 
-export default home
+export default Navbar
